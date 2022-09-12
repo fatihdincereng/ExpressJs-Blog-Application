6acde7c5cab8799f21542129a45e1e3b6455c68e
Fatih Dincer <88143919+fatihdincereng@users.noreply.github.com>
2022-09-12 23:29:13 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-// Kayit Ol Get ve Post İişlemlerine İhtiyacın Var.
 
 router.get('/register', (req, res) => {
     res.render('site/register');
@@ -28,17 +27,12 @@ router.get('/logout', (req, res) => {
     })
 });
 
-// Kullanıcı Kontrolü Çok Önemli Kısım
-// 1.Aşama Olarak Değer Kontrolü Yapacağın için frontendden req.bodyden email ve password değerlerini alman lazım daha sonra veritabanındakilerle karşılaştırıp işlem yapman gerekir.
 
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
-    // FrontEnddeki veriye göre arama yaparız Algoritma Mantığıdır Bu.
     User.findOne({ email }, (err, user) => {
-        // Userda Gelen Verinin bilgileri nesne halinde tutulur
         if (user) {
             if (user.password == password) {
-                // User Session İşlemi Yapılacak.
                 req.session.UserId=user._id;
                 res.redirect('/')
             }
