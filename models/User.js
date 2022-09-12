e24eb7930c14515d3f420a4ee2a6a43ff32ea7c9
Fatih Dincer <88143919+fatihdincereng@users.noreply.github.com>
2022-09-12 23:28:09 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 
-//MongoDb Deki vERİTABANININ Yapısını belirleriz.
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true ,unique:true},
     email: { type: String, required: true ,unique:true },
